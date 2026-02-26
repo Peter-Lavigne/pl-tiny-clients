66104a0eccd010b5e6078782829bb419bc60a2f0
Peter-Lavigne <peterklavigne@gmail.com>
2026-02-26 07:05:58 -0500

    Bump version to 0.0.23

---
+++ src/pl_tiny_clients/placeholder.py
@@ -0,0 +1,2 @@
+def placeholder() -> None:
+    pass
---
+++ tests/placeholder_test.py
@@ -0,0 +1,6 @@
+from src.pl_tiny_clients.placeholder import placeholder
+
+
+def test_placeholder() -> None:
+    placeholder()
+    assert True
